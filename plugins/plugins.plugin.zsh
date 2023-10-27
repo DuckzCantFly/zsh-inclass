e9fc2173bbdf0a3430376b32105fd9ceaa6fe944
DuckzCantFly <Duckz@protonmail.ch>
2023-10-27 01:48:35 -0600

    fix comments

--- plugins/plugins.plugin.zsh
+++ plugins/plugins.plugin.zsh
@@ -1,5 +1,6 @@
 ### Plugins & Plugin Config ###
 
+# Function load plugins and configs
 _zplug_load(){
 	if [ ! -d "${ZPLUGINDIR}/${2}" ]; then
 		git clone --quiet "https://github.com/${1}/${2}" "${ZPLUGINDIR}/${2}"
@@ -7,30 +8,27 @@ _zplug_load(){
 	source ${ZPLUGINDIR}/${2}/*.plugin.zsh
 }
 
-
-# Function load plugins and configs
-
-## Zsh-Completions
-### get more tab completion options
+# Zsh-Completions
+## get more tab completion options
 _zplug_load zsh-users zsh-completions
 
-## Zsh-History-Substring-Search
-### search for previous comands in the terminal
+# Zsh-History-Substring-Search
+## search for previous comands in the terminal
 _zplug_load zsh-users zsh-history-substring-search
 
 	bindkey '^[[A' history-substring-search-up
 	bindkey '^[[B' history-substring-search-down
 
-## Zsh-Autosuggestions
-### Zsh guesses next command
+# Zsh-Autosuggestions
+## Zsh guesses next command
 _zplug_load zsh-users zsh-autosuggestions
 
 	ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion)
 	# ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 	# bindkey '<M-Space>' autosuggest-accept
 
-## Zsh-You-Should-Use
-### zsh reminds you of alises
+# Zsh-You-Should-Use
+## zsh reminds you of alises
 _zplug_load MichaelAquilina zsh-you-should-use
 
 	export YSU_MESSAGE_POSITION="after"	# Display after output 
@@ -48,10 +46,10 @@ _zplug_load MichaelAquilina zsh-you-should-use
 	export YSU_IGNORED_ALIASES=(
 	) # Disaable on commands
 
-## Fast-Syntax-Highlighting
-### Colors typed txt so output looks better
+# Fast-Syntax-Highlighting
+## Colors typed txt so output looks better
 _zplug_load zdharma-continuum fast-syntax-highlighting
 
-## Zsh-System-Clipboard
-### Better clipboad Intergration
+# Zsh-System-Clipboard
+## Better clipboad Intergration
 _zplug_load kutsan zsh-system-clipboard
