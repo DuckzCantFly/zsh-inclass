a578b8fb5c7f81bd27f94774474a4cfd366044ec
DuckzCantFly <Duckz@protonmail.ch>
2023-10-27 02:24:35 -0600

    Added comments

--- correction/correction.plugin.zsh
+++ correction/correction.plugin.zsh
@@ -1,18 +1,21 @@
 ### Nyae Correction ###
+# Sets up autocorection formating #
 
 # enable correction
 setopt CORRECT
 
+# Setup for each basic element
 function _correction-prompt-elements(){
 	nl="\e[0m${fg[blue]}"
 	nb="${fg_bold[blue]}"
-	start="\e[0m${fg_bold[red]}]\e[0m"
+	start=""
 	error="${fg[red]}%R\e[0m"
 	fix="${fg_bold[green]}%r\e[0m"
 	arrow="->"
 	errormsg="${fg[red]}Syntax Error:\e[0m"
 }
 
+# Layout for long version of prompt.
 function _correction-prompt-verbose(){
 	local nl nb start error fix arrow errormsg
 	_correction-prompt-elements
@@ -23,6 +26,7 @@ function _correction-prompt-verbose(){
 	SPROMPT="$( echo ${_sprompt} )"
 }
 
+# Layout for medium version of prompt.
 function _correction-prompt-medium(){
 	local nl nb start error fix arrow errormsg
 	_correction-prompt-elements
@@ -30,20 +34,22 @@ function _correction-prompt-medium(){
 	local nyae="${nb}N${nl}|${nb}y${nl}|${nb}a${nl}|${nb}e\e[0m"
 	# local nyae="${nb}Nyae\e[0m"
 	_sprompt="${start} Replace '${error}' with '${fix}' [${nyae}]: "
-	# local sprompt="${start} '${error}' ${arrow} '${fix}' [${nyae}]: "
-	# local sprompt="${start} Replace '${error}' '${fix}'?: "
+	# local _sprompt="${start} '${error}' ${arrow} '${fix}' [${nyae}]: "
+	# local _sprompt="${start} Replace '${error}' '${fix}'?: "
 	SPROMPT="$( echo ${_sprompt} )"
 }
 
+# Layout for short version of prompt.
 function _correction-prompt-short(){
 	local nl nb start error fix arrow errormsg
 	_correction-prompt-elements
-	# local sprompt="${start}\"${error}\" ${arrow} \"${fix}\": "
-	# local sprompt="${start}'${error}' ${arrow} '${fix}': "
+	# local _sprompt="${start}\"${error}\" ${arrow} \"${fix}\": "
+	# local _sprompt="${start}'${error}' ${arrow} '${fix}': "
 	_sprompt="${start} ${error} ${arrow} ${fix}: "
 	SPROMPT="$( echo ${_sprompt} )"
 }
 
+# Which layout should we use?
 function _correction-set(){
 	case "${ZSH_DOT_CONFIG_CORRECTION}" in
 		verbose)	_correction-prompt-verbose ;;
